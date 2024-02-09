d8de1c481a3bd13178acf162f371b79519dd7757
Natalia Benko <characteronme@gmail.com>
2024-02-09 17:27:08 +0200

    feat: main template and styling;

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { CurrencyService } from '../services/currency.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule, MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  data: any[] = [];
+  currencies: string[] = ["EUR", "USD", "UAH"];
+  firstCurrency = 1;
+  secondCurrency = signal(0);
 
+  constructor(private currencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.currencyService.getData().subscribe((currencies) => {
+       this.data = currencies;
+   });
+ }
 }
